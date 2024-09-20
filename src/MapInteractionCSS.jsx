1b35edecde96f8922777b2e0fac31019b9175b42
evejk <mail@evelinkremer.de>
2024-09-20 13:26:18 +0200

    refactor(MapInteractionsCSS): add transition

--- src/MapInteractionCSS.jsx
+++ src/MapInteractionCSS.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import MapInteraction from './MapInteraction';
+import React from "react";
+import MapInteraction from "./MapInteraction";
 
 /*
   This component provides a map like interaction to any content that you place in it. It will let
@@ -8,38 +8,37 @@ import MapInteraction from './MapInteraction';
 const MapInteractionCSS = (props) => {
   return (
     <MapInteraction {...props}>
-      {
-        ({ translation, scale }) => {
-          // Translate first and then scale.  Otherwise, the scale would affect the translation.
-          const transform = `translate(${translation.x}px, ${translation.y}px) scale(${scale})`;
-          return (
+      {({ translation, scale }) => {
+        // Translate first and then scale.  Otherwise, the scale would affect the translation.
+        const transform = `translate(${translation.x}px, ${translation.y}px) scale(${scale})`;
+        return (
+          <div
+            style={{
+              height: "100%",
+              width: "100%",
+              position: "relative", // for absolutely positioned children
+              overflow: "hidden",
+              touchAction: "none", // Not supported in Safari :(
+              msTouchAction: "none",
+              cursor: "all-scroll",
+              WebkitUserSelect: "none",
+              MozUserSelect: "none",
+              msUserSelect: "none",
+            }}
+          >
             <div
               style={{
-                height: '100%',
-                width: '100%',
-                position: 'relative', // for absolutely positioned children
-                overflow: 'hidden',
-                touchAction: 'none', // Not supported in Safari :(
-                msTouchAction: 'none',
-                cursor: 'all-scroll',
-                WebkitUserSelect: 'none',
-                MozUserSelect: 'none',
-                msUserSelect: 'none'
+                display: "inline-block", // size to content
+                transform: transform,
+                transformOrigin: "0 0",
+                transition: "transform 0.3s ease",
               }}
             >
-              <div
-                style={{
-                  display: 'inline-block', // size to content
-                  transform: transform,
-                  transformOrigin: '0 0 '
-                }}
-              >
-                {props.children}
-              </div>
+              {props.children}
             </div>
-          );
-        }
-      }
+          </div>
+        );
+      }}
     </MapInteraction>
   );
 };
